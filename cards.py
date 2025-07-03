a21bc7783b859079e4f5f37a5b04922f79cb3953
MoCoXIII <112095580+MoCoXIII@users.noreply.github.com>
2025-07-04 01:17:08 +0200

    Fix default value assignment for nextColor in Card class constructor

--- cards.py
+++ cards.py
@@ -1,9 +1,9 @@
 class Card:
-    def __init__(self, name, add=0, mult=1, affects=[1], color=None, nextColor=color, image="uno.png"):
+    def __init__(self, name, add=0, mult=1, affects=[1], color=None, nextColor=None, image="uno.png"):
         self.name = name
         self.add = add
         self.mult = mult
         self.affects = affects
         self.color = color
-        self.nextColor = nextColor
+        self.nextColor = nextColor if nextColor is not None else color
         self.image = image
