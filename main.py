92de9f6c7d99b3967078cd15b8458b86b3e420e8
MoCoXIII <112095580+MoCoXIII@users.noreply.github.com>
2025-07-25 18:35:40 +0200

    refactor deckImage generation into visuals.py

--- main.py
+++ main.py
@@ -7,52 +7,19 @@ import sys, os
 # This makes sure the parent directory is on path, allowing imports from inside the infinUno package, while also allowing other scripts to use this folder as a package.
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from . import cards
-from . import visuals
+from infinUno import cards, visuals, players # type: ignore
 
 pygame.init()
 
 
-def show_deck(window, deck, FRAME):
-    for x in range(len(deck.cards)):
-        card_spread = (
-            0
-            if len(deck.cards) == 1
-            else (
-                (window.width - 20 - FRAME.get_width()) / (len(deck.cards) - 1)
-                if len(deck.cards) * FRAME.get_width() > window.width * 2 - 20
-                else FRAME.get_width() // 2
-            )
-        )
-
-        window.showCard(
-            x=(
-                window.width // 2
-                - ((len(deck.cards) - 1) * card_spread + FRAME.get_width()) // 2
-            )
-            + x * card_spread,
-            y=(
-                window.height
-                - window.height // 2
-                + window.height // 7
-                + ((x - ((len(deck.cards) - 1) / 2)) ** 2)
-                * (80 / ((max((len(deck.cards) - 1), 1) / 2) ** 2))
-                if window.height - window.height // 2 + window.height // 7 > 1
-                else 0
-            ),
-            angle=(
-                0
-                if len(deck.cards) == 1
-                else ((len(deck.cards) - 1) / 2 - x) * (2 * 20) / (len(deck.cards) - 1)
-            ),
-            card=deck.cards[x],
-        )
+def show_deck(window, deck):
+    deckImage = visuals.deckImage(window, deck)
+    window.window.blit(deckImage, (window.width // 2 - deckImage.get_width() // 2, window.height // 2 - deckImage.get_height() // 2))
 
 
 if __name__ == "__main__":
     window = visuals.Window("InfinUno")
     deck = cards.Deck()
-    FRAME = pygame.image.load("resources/cards/frame.png").convert_alpha()
     running = True
     while running:
         window.window.fill((0, 0, 0))
@@ -70,7 +37,7 @@ if __name__ == "__main__":
             deck.clear()
             for x in range(10):
                 deck.draw()
-        show_deck(window, deck, FRAME)
+        show_deck(window, deck)
         # PER FRAME CODE HERE
 
         pygame.display.flip()
--- visuals.py
+++ visuals.py
@@ -6,13 +6,14 @@ import sys, os
 # read this comment in main.py for more info
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from infinUno.cards import ALL_CARDS # type: ignore
+from infinUno.cards import ALL_CARDS  # type: ignore
+from infinUno import cards  # type: ignore
 
 pygame.init()
 
 
 class Window:
-    def __init__(self, name):
+    def __init__(self, name, window=None):
         self.width = pygame.display.Info().current_w
         self.height = pygame.display.Info().current_h
         self.name = name
@@ -20,13 +21,15 @@ class Window:
         self.window = pygame.display.set_mode(
             (pygame.display.Info().current_w, pygame.display.Info().current_h),
             pygame.SCALED,
-        )
+        ) if window is None else window
         pygame.display.set_caption(self.name)
         pygame.display.set_icon(self.icon)
         self.running = False
 
-    def showCard(self, x, y, angle, card):
+    def showCard(self, x, y, angle, card, surface=None):
         # Display a card image at (x, y). If x or y is "center", center the card.
+        if surface is None:
+            surface = self.window
         card_img = card.image
         card_rect = card_img.get_rect()
         if x == "center":
@@ -39,7 +42,47 @@ class Window:
             card_rect.y = y
         rotated_card = pygame.transform.rotate(card_img, angle)
         rotated_card_rect = rotated_card.get_rect(center=card_rect.center)
-        self.window.blit(rotated_card, rotated_card_rect)
+        surface.blit(rotated_card, rotated_card_rect)
+
+
+def deckImage(window, deck):
+    FRAME = cards.loadResource("resources/cards/frame.png").convert_alpha()
+    surface = pygame.Surface(window.window.get_size(), pygame.SRCALPHA)
+    for x in range(len(deck.cards)):
+        card_spread = (
+            0
+            if len(deck.cards) == 1
+            else (
+                (window.width - 20 - FRAME.get_width()) / (len(deck.cards) - 1)
+                if len(deck.cards) * FRAME.get_width() > window.width * 2 - 20
+                else FRAME.get_width() // 2
+            )
+        )
+
+        window.showCard(
+            x=(
+                window.width // 2
+                - ((len(deck.cards) - 1) * card_spread + FRAME.get_width()) // 2
+            )
+            + x * card_spread,
+            y=(
+                window.height
+                - window.height // 2
+                + window.height // 7
+                + ((x - ((len(deck.cards) - 1) / 2)) ** 2)
+                * (80 / ((max((len(deck.cards) - 1), 1) / 2) ** 2))
+                if window.height - window.height // 2 + window.height // 7 > 1
+                else 0
+            ),
+            angle=(
+                0
+                if len(deck.cards) == 1
+                else ((len(deck.cards) - 1) / 2 - x) * (2 * 20) / (len(deck.cards) - 1)
+            ),
+            card=deck.cards[x],
+            surface=surface,
+        )
+    return surface
 
 
 if __name__ == "__main__":
